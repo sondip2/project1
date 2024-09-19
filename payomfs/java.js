d4655d74dc1f7772aa5e99d4bdeba3699c6b241f
sondip2 <sarkarsondip103@gmail.com>
2024-09-19 16:30:03 +0600

    first commit

---
+++ payomfs/java.js
@@ -0,0 +1,8 @@
+// step 1 set event handler
+document.getElementById('button-login').addEventListener('click', function(eventt){
+eventt.preventDefault();
+console.log('login button clicked');
+
+const phoneNumber = document.getElementById('phone-number').Value
+console.log(phoneNumber)
+})
