f4ec8a35c41ae5e366439669fdc9250169330331
sondip2 <sarkarsondip103@gmail.com>
2024-09-20 17:08:46 +0600

    first commit

--- payomfs/java.js
+++ payomfs/java.js
@@ -1,8 +1,37 @@
-// step 1 set event handler
-document.getElementById('button-login').addEventListener('click', function(eventt){
-eventt.preventDefault();
-console.log('login button clicked');
+// console.log('button clicking added');
 
-const phoneNumber = document.getElementById('phone-number').Value
-console.log(phoneNumber)
+// search: form submit reloading the  page 
+
+// step - 1.set event handler
+document.getElementById('login-btn').addEventListener('click', function(event){
+    // step-2: prevent default behavior
+    event.preventDefault(); 
+    // console.log('login button clicked'); 
+
+    // get the phone number
+
+const phoneNumber = document.getElementById('phone-number').value;
+
+const pinNumber = document.getElementById
+('pin-number').value;
+// console.log(phoneNumber, pinNumber);
+
+// step4: validate phone and pin
+// this is very temporary way 
+
+if(phoneNumber=== '5' && pinNumber === '1234'){
+    console.log('you are logged in');
+    window.location.href ="/home.html";
+   
+   
+
+}
+else{
+    alert('wrong phone number or pin');
+}
+
+
+
 })
+
+
